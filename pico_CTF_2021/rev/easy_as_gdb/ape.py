9fafa915601ed99ac652a6b8f6791fa0a44b5929
Jefferson Ding <tyding2004@outlook.com>
2021-03-29 00:29:32 -0400

    march 29th, easy as gdb using angr

---
+++ pico_CTF_2021/rev/easy_as_gdb/ape.py
@@ -0,0 +1,29 @@
+import angr
+import claripy
+
+
+char_length = 35
+
+success = 0x00010a72
+fail = 0x00010a86
+base_addr = 0x00010000
+
+project = angr.Project('./brute', main_opts={"base_addr": base_addr})
+
+input_chars = [claripy.BVS("char_{}".format(i), 8) for i in range(char_length)]
+
+flag = claripy.Concat(*input_chars + [claripy.BVV(b'\n')])
+
+state = project.factory.full_init_state(
+    args=['./brute'],
+    add_options=angr.options.unicorn,
+    stdin=flag
+)
+
+sim_manager = project.factory.simulation_manager(state)
+
+sim_manager.explore(find=success, avoid=fail)
+
+if len(sim_manager.found) > 0:
+    for found in sim_manager.found:
+        print(found.posix.dumps(0))
